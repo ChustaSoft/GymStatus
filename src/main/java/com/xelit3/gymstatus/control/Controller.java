e27d636d5f30ac5a4ff916184a1314510285f721
Xavi Rueda <javiru87@gmail.com>
2015-03-04 01:12:12 +0100

    Creation of singleton and related resources. Not finished yet

--- src/main/java/com/xelit3/gymstatus/control/Controller.java
+++ src/main/java/com/xelit3/gymstatus/control/Controller.java
@@ -19,6 +19,8 @@ public class Controller {
 	public void startApp(){
 		view = new MainWindow(this);
 		view.setVisible(true);
+		
+		//TODO Load AppSettings singleton
 	}
 	
 	public boolean saveFitnessExercise(){
---
+++ src/main/java/com/xelit3/gymstatus/control/settings/AppSettings.java
@@ -0,0 +1,74 @@
+package com.xelit3.gymstatus.control.settings;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AppSettings {
+	
+	private static final String SETTINGS_FILE = "src/main/resources/app_config.txt";
+	private static final String USERNAME_SETTING = "USERNAME";
+	private static final String LANGUAGE_SETTING = "LANGUAGE";
+	
+	public static AppSettings singleton;
+	
+	public String username;
+	public String language;
+	
+	private AppSettings(){
+		singleton = new AppSettings();
+		try(BufferedReader br = new BufferedReader(new FileReader(AppSettings.SETTINGS_FILE))) {
+	        String line = br.readLine();
+	        
+	        while (line != null) {
+	            String []tmpConfig = line.split(":");
+	            
+	            switch(tmpConfig[0]){
+	            case AppSettings.USERNAME_SETTING:
+	            	singleton.setUsername(tmpConfig[1]);
+	            	break;
+	            	
+	            case AppSettings.LANGUAGE_SETTING:
+	            	singleton.setLanguage(tmpConfig[1]);
+	            	break;
+	            }
+	            
+	            line = br.readLine();
+	        }
+	        
+	        
+	    } 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}		
+		
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+	public static AppSettings getInstance(){
+		if(singleton == null)
+			singleton = new AppSettings();
+		
+		return singleton;
+	}
+	
+}
